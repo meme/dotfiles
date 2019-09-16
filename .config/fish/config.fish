7aafb5e8931a9defad857f1ebee4278f229f984f
meme <meme@users.noreply.github.com>
2019-09-15 20:44:42 -0400

    Add Binary Ninja and IDA script

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,18 @@ set -gx GOPATH $HOME/go
 
 alias "gg = gdb -q"
 
+function ida
+    ~/.wine/drive_c/Program\ Files/IDA\ 7.0/ida.exe $argv > /dev/null 2>&1 & disown
+end
+
+function ida64
+    ~/.wine/drive_c/Program\ Files/IDA\ 7.0/ida64.exe $argv > /dev/null 2>&1 & disown
+end
+
+function bn
+    binaryninja $argv > /dev/null 2>&1 & disown
+end
+
 function e
   if [ (count $argv) -gt 0 ]
     env TERM=xterm $EDITOR $argv
